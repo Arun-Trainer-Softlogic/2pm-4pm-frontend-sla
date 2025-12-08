27366af23c852f2f593fbb446579bcad9c9d37a5
arunsla <arunraj.softlogic@gmail.com>
2025-12-08 14:09:32 +0530

    js day 1

---
+++ js/Day 1/intro.js
@@ -0,0 +1,72 @@
+// wat does js do :
+
+// change html element
+// change css style
+// respond to button clicks
+// validate forms
+// create annimations
+// build full apps (react , node.js ,etc )
+
+// where js runs
+// browers  =. chrome , edge , firefoxes , safari suite , brave, opera , onion tor
+// Node.js = runs js coutside the browser
+
+// Js engine
+
+// chrome - v8 engine
+// firefox = spidermonkey
+// safari => javascript core
+
+// reads the js code line by line
+
+/*
+console.log("arun is the best ");
+console.log(25);
+console.log(true);
+console.log({name : "arun" , age : 23, });
+*/
+
+// function ar(){
+//     alert('hello')
+// }
+
+// let , var , const
+
+// var a = 10;
+
+// a = 40 ; // reassignment
+
+// var a = 30; // redeclaring
+
+// {
+//     var a = 70;
+// }
+
+// console.log(a);
+
+// let b = 40;
+// b = 56; // reassigng is possible in let
+
+// b = 89;
+
+// {
+//   let x = 90;
+//   console.log(b);
+// }
+
+// let x = 77;
+// console.log(x);
+
+// // let b = 98; // redeclaring is  not posssibl e
+
+// console.log(b);
+// const ar = 90
+
+
+// {
+//     const ar =88;
+//     console.log(ar);
+    
+// }
+// console.log(ar);
+

04cda6ea09536e4b9b448083b9d6f963682103dc
arunsla <arunraj.softlogic@gmail.com>
2025-12-12 17:06:18 +0530

    DAY 6

---
+++ js/Day 6/array.js
@@ -0,0 +1,94 @@
+
+
+// let numbers = [1,2,3,4,5,6]
+
+
+// let mixedValues = ["arun", 32, true]
+
+// accesing the array values 
+// console.log(fruits[4]);
+
+
+
+
+// console.log(numbers);
+// console.log(numbers[2]);
+ 
+// push()
+
+// fruits.push("orange");
+// fruits.pop();
+// // fruits.pop();
+
+// console.log(fruits);
+
+
+// let originalValue = [10,20,30,40,50,60 ] ;
+// const valueRemove = 30;
+// const newArray = [];
+
+
+
+// for(let i = 0 ; i < originalValue.length; i++){
+//     if(originalValue[i] !== valueRemove ){
+//         newArray.push(originalValue[i])
+//     }
+// }
+
+
+// let fruits = ["apple", "banana", "mesabi", "mango" , "pine"   ];
+
+// fruits.pop();
+// fruits.shift();
+// fruits.unshift("Grapes");
+
+
+// console.log(fruits);
+
+
+
+
+// nums.forEach( n => console.log(n))
+
+
+// let Doubled = nums.map(n => n* 2);
+
+// console.log(Doubled);
+
+
+
+// let nums = [1,2,3]
+// let evens = nums.filter(n => n % 2 === 0);
+// console.log(evens); 
+
+
+
+
+
+// let matrix = [[1, 2, 3],[4, 5, 6],[7, [44,55,66,[9999]], 9]];
+
+
+// console.log(matrix[2][1][3][0]);
+
+
+// 230
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

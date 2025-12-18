c679de03a77e57181a815d0cbd1ba53d69b61404
arunsla <arunraj.softlogic@gmail.com>
2025-12-18 15:40:36 +0530

    upate

---
+++ js/Day 9/Errors.js
@@ -0,0 +1,79 @@
+// console.log("hello raguram";
+
+// let x = 10;
+
+// console.log(x);
+// console.log(y);
+
+// let num = 5 ;
+// num();
+
+// let a = 10 ;
+// let b = 5 ;
+// console.log(a+b);
+
+// try {
+//     // code block
+
+// }catch {
+//     // code block
+
+// } finally{
+//     // code block // always run
+// }
+
+// try {
+//     // risky code
+// } catch (error){
+//     // runs if code error occurs
+// } finalyy {
+//     // always run
+
+// }
+
+// try {
+//     // let x = 90;
+//     let result = x + 10 ;
+//     console.log(result);
+
+// }catch (e){
+//     console.log("error has Occured : ", e.stack);
+
+// } finally {
+//     console.log("code ran successfully");
+
+// }
+
+// try {
+//     let x = y + 5
+// }catch (e){
+//     console.log(e.name);
+//     console.log(e.message);
+// }
+
+// throw new Error("Message");
+
+// let marks = 15 ;
+
+// if(age < 15 ){
+//     throw new Error("You are not eligible to vote ")
+// }
+
+
+
+// let a = 20 ;
+// try {
+//   let marks = -10;
+
+//   if (marks < 10) {
+//     throw new Error("marks cannot be negative");
+//   }
+//   console.log("Marks : ", marks);
+// } catch (error) {
+//   console.log("custom Error : ", error.message);
+// }finally{
+//     console.log("code ran fully ");
+    
+// }
+
+// console.log(b);

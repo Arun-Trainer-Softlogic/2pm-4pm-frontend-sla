b41df3431e06e903a6103874b5e5f852739d2592
arunsla <arunraj.softlogic@gmail.com>
2026-02-05 17:22:53 +0530

    update

--- React/ars/src/app/store.js
+++ React/ars/src/app/store.js
@@ -1,1 +1,12 @@
-import { config} from '@redux/toolkit'
+import { configureStore } from '@redux/toolkit'
+
+
+
+import counterReducer from '../features/counter/counterSlice'
+import reducer from '../features/counter/counterSlice'
+
+export const store = configureStore({
+reducer : {
+    counter : counterReducer
+}
+})
